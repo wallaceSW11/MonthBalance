49a2fcf577b4427477e589667752aca25e954273
wallace-deeprocket <wallace@deeprocket.com.br>
2026-02-16 21:45:07 -0300

    atualização do site

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,11 +1,11 @@
-// Inicializa AOS (Animate On Scroll)
+
 AOS.init({
     duration: 1000,
     once: true,
     offset: 100
 });
 
-// Fechar menu mobile ao clicar em um link
+
 const navbarToggler = document.body.querySelector('.navbar-toggler');
 const responsiveNavItems = [].slice.call(
     document.querySelectorAll('#navbarNav .nav-link')
